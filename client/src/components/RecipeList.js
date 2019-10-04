f97b284f454ce2dba5234e308054e90c2af157b2
Bryan <bryan@fiddle.io>
2019-10-04 11:17:26 +0800

    minor style changes

--- client/src/components/RecipeList.js
+++ client/src/components/RecipeList.js
@@ -1,18 +1,13 @@
-import React, { Component } from "react";
-import { Card } from "react-bootstrap";
-import Img from "../img/temp-food.jpg";
- 
+import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
+import Img from '../img/temp-food.jpg';
+
 export default class RecipeList extends Component {
   render() {
-    const {
-      name, 
-      category,
-      description,
-      instructions,
-    } = this.props.recipe;
+    const { name, category, description, instructions } = this.props.recipe;
     return (
-      <div class="container">
-        <Card style={{ width: "20rem" }}>
+      <div class="card-container">
+        <Card style={{ minWidth: '260px', width: '100%' }}>
           <Card.Img variant="top" src={Img} alt="Logo" background="center" />
           <Card.Body>
             <Card.Title>
