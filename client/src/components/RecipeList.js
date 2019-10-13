f5af9f044f55cabcf8de96f40c8156072efd18cb
Bryan <bryan@fiddle.io>
2019-10-13 09:33:18 +0800

    added users

--- client/src/components/RecipeList.js
+++ client/src/components/RecipeList.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import Img from '../img/temp-food.jpg';
 import style from './recipe.module.css';
 
-export default  function RecipeList ({recipe}){
-    const { name, description } = recipe;
-    return (
+export default function RecipeList({ recipe }) {
+  const { name, description, username } = recipe;
+  return (
     <div className={style.recipe}>
-      <img className={style.image} src={Img} alt=""/>
-      <h3 className={style.name}>{name}</h3>
-      <p className={style.description}>{description}</p>
+      <div className={style.imagecontainer} />
+      <div className={style.lowersection}>
+        <h3 className={style.name}>{name}</h3>
+        <p className={style.description}>{description}</p>
+        <div className={style.profile}>
+          <span className={style.image}>
+            <img src="/img/temp-profile-pic.jpg" alt="profile" />
+          </span>
+          <span className={style.by}>By: </span>
+          <span className={style.username}>{`${username}`}</span>
+        </div>
+      </div>
     </div>
-    );
-  }
-
+  );
+}
